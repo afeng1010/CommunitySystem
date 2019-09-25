42f1d80f63567573f4853897b5c991571b55899f
WeiJie Xu <51219678+afeng1010@users.noreply.github.com>
2019-09-25 16:21:01 +0800

    Add files via upload

---
+++ communitysystem/src/main/java/com/communitysystem/CommunitysystemApplication.java
@@ -0,0 +1,13 @@
+package com.communitysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunitysystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunitysystemApplication.class, args);
+    }
+
+}
